00514eb3d7c861b09ed2b6263d8b8d6d5c1bd96e
sidparkar <siddhi.parkar95@gmail.com>
2018-10-05 11:13:20 +0530

    footer.php

---
+++ footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-sm-4 col-xs-12 matter">
+                <img src="images/logo.png" class="img-responsive" alt="logo" title="logo" />
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc est justo, aliquam nec tempor fermentum, commodo et libero. Quisque et rutrum arcu. Vivamus dictum tincidunt magna id euismod. Nam sollicitudin mi quis orci lobortis feugiat.</p>
+
+            </div>
+            <div class="col-md-3 col-sm-3 col-xs-12 info">
+                <h5>Usefull Links</h5>
+				<ul class="list-inline links">
+                    <li><a href="index-2.html">Home</a></li>
+					<li><a href="price_plan.html">Pricing Plans</a></li>
+                    <li><a href="aboutus.html">About</a></li>
+					 <li><a href="faq.html">FAQ</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+					<li><a href="all_ads.html">All Ads</a></li>
+                    <li><a href="login.php">Login</a></li>
+					<li><a href="contactus.html">Help & Contact us</a></li>
+                    <li><a href="register.php">Register</a></li>
+                    <li><a href="terms_and_conditions.html">Terms & Conditions</a></li>
+                </ul>
+            </div>
+            <div class="col-md-4 col-sm-4 col-xs-12">
+                <h5>Contact with us</h5>
+				<ul class="list-inline social">
+                    <li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+                    <li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="https://plus.google.com/" target="_blank"><i class="social_googleplus" aria-hidden="true"></i></a></li>
+                    <li><a href="https://www.instagram.com/" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+					<li><a href="https://www.youtube.com/" target="_blank"><i class="social_youtube"></i></a></li>
+                    <li><a href="https://in.linkedin.com/" target="_blank"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+	<div class="powered">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-12 col-xs-12 text-center">
+					<p>© 2017 <span>Classifiedspot</span>. All rights reserved. Theme by <span>TMD Studio</span></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
